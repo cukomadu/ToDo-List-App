345f6298ca61d99994a24daa8687fa9be0f66bf4
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-03 17:24:07 -0500

    Initialized second react component - alltasksview

---
+++ src/scripts/react-components/allTasksView.js
@@ -0,0 +1,18 @@
+//Import all standard libraries referenced in this file
+import React from 'react'
+
+
+//Create All Tasks React Component
+const AllTasksView = React.createClass({
+	render: function(){
+		return (
+			)
+	}
+	
+
+	
+})
+
+
+//Export this file so its available to app.js
+export default AllTasksView

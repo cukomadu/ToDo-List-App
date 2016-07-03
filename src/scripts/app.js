3fbf594a010caa94243c90938f086eca91aeb23f
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-03 16:36:14 -0500

    Created react component folder under src/scripts folder and 4 .js files under react component folder for todolistview, alltasksview, doneview, and undoneview

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'	
 
-const app = function() {
-
-	const Header = React.createClass({
-		render: () => {
-			
-		}
-	})
-
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
-}
-
-app()
---
+++ src/scripts/react components/toDoListView.js
@@ -0,0 +1,1 @@
+toDoListView.js

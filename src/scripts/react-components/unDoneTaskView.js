9d1c757cdc93b4f65868ec381bef21934f0318da
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-03 17:27:36 -0500

    Initialized fourth react component - undonetasksview

---
+++ src/scripts/react-components/unDoneTaskView.js
@@ -0,0 +1,18 @@
+//Import all standard libraries referenced in this file
+import React from 'react'
+
+
+//Create All Tasks React Component
+const unDoneTasksView = React.createClass({
+	render: function(){
+		return (
+			)
+	}
+	
+
+	
+})
+
+
+//Export this file so its available to app.js
+export default unDoneTasksView

6f49a62207eb87a13ad15e3fcf62446ea7f89726
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-03 17:19:27 -0500

    Initialized main react component - todolistView

--- src/scripts/react-components/toDoListView.js
+++ src/scripts/react-components/toDoListView.js
@@ -1,1 +1,27 @@
-toDoListView.js
+//Import all standard libraries referenced in this file
+import React from 'react'
+
+
+//Import all custom files referenced in this file
+import AllTasksView from 'allTasksView.js'
+import DoneTasksView from 'doneTasksView.js'
+import UndoneTasksView from 'undoneTasksView.js'
+
+
+//Create Main React Component
+
+const ToDoListView = React.createClass({
+	render: function(){
+		return (
+			)
+	}
+	
+
+	
+})
+
+
+//Export this file so its available to app.js
+export default ToDoListView
+
+

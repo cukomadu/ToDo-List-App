0b9ed8a906073aa3936856f3cb94040c476e9311
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-03 17:02:59 -0500

    Imported all standard libraries/custom js files needed and completed initial Backbone Setup for TodolistRouter, TodolistCollection, and TodolistModel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,35 @@
+//Standard Libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
 
+//Custom .js Files
+import ToDoListView from './react-components/toDoListView.js'
+
+
+//Todo List Model
+var ToDoListModel = Backbone.Model.extend({
+
+})
+
+
+
+//Todo List Collection
+var ToDoListCollection = Backbone.Collection.extend({
+
+})
+
+
+
+//App Router Function
+var ToDoListRouter = Backbone.Router.extend({
+
+
+initialize: function(){
+	Backbone.history.start()
+}
+
+})
+
+
+new ToDoListRouter ()
